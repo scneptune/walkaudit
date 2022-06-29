bc64c79ff35c54a823e2268a72f772e3980ed510
Steven Neptune <steven.neptune@native-instruments.com>
2022-06-30 00:00:15 +0200

    initial setup and configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
+    dest: "public",
+    register: true,
+    skipWaiting: true,
     runtimeCaching,
   },
-})
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+});
