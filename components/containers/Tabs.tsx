8dfeda9ec31c3df8739a9878f3417a9847ea9df4
Steven Neptune <steven.neptune@native-instruments.com>
2022-06-30 00:02:09 +0200

    Tab Component for Forms

---
+++ components/containers/Tabs.tsx
@@ -0,0 +1,73 @@
+import React, { MutableRefObject } from "react";
+
+import { useTab, useTabList, useTabPanel } from "react-aria";
+import { useTabListState } from "@react-stately/tabs";
+import { TabListProps } from "@react-types/tabs";
+import styled from "@emotion/styled";
+
+export function Tabs(props: TabListProps<T>) {
+  let state = useTabListState(props);
+  let ref: MutableRefObject<T> = React.useRef();
+  let { tabListProps } = useTabList(props, state, ref);
+  return (
+    <div>
+      <TabListWrapper {...tabListProps} ref={ref}>
+        {Array.from(state.collection).map((item) => (
+          <Tab key={item.key} item={item} state={state} />
+        ))}
+      </TabListWrapper>
+      <TabPanel key={state.selectedItem?.key} state={state} />
+    </div>
+  );
+}
+
+export function Tab({ item, state }: { item: any; state: any }) {
+  let { key, rendered } = item;
+  let ref: MutableRefObject<T> = React.useRef();
+  let { tabProps } = useTab({ key }, state, ref);
+  let isSelected = state.selectedKey === key;
+  let isDisabled = state.disabledKeys.has(key);
+  return (
+    <TabItemWrapper
+      {...tabProps}
+      isSelected={isSelected}
+      isDisabled={isDisabled}
+      ref={ref}
+    >
+      {rendered}
+    </TabItemWrapper>
+  );
+}
+
+export function TabPanel({ state, ...props }) {
+  let ref: MutableRefObject<T> = React.useRef();
+  let { tabPanelProps } = useTabPanel(props, state, ref);
+  return (
+    <TabPanelWrapper {...tabPanelProps} ref={ref}>
+      {state.selectedItem?.props.children}
+    </TabPanelWrapper>
+  );
+}
+
+type TabItemWrapperProps = {
+  isDisabled?: boolean;
+  isSelected: boolean;
+};
+
+const TabListWrapper = styled("ul")`
+  list-display-type: none;
+  padding-left: 0;
+  display: flex;
+  border-bottom: 1px solid grey;
+`;
+
+const TabItemWrapper = styled("li")<TabItemWrapperProps>`
+  padding: 10px;
+  opacity: ${({ isDisabled }) => (isDisabled ? 0.5 : undefined)};
+  border-bottom: ${({ isSelected }) =>
+    isSelected ? "3px solid #222222" : undefined};
+`;
+
+const TabPanelWrapper = styled("div")`
+  padding: 10px;
+`;
