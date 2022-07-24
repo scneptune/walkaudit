9a1f9a96819de990cb582277cdad3ef37bffb133
Steven Neptune <steven.neptune@native-instruments.com>
2022-07-24 10:13:57 -0500

    add in prisma and next-auth dependencies, added some more icons to the svg.

---
+++ global.d.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    var prisma: PrismaClient;
+        namespace NodeJS {
+            interface ProcessEnv {
+                GOOGLE_APPLICATION_CREDENTIALS: string;
+                SMTP_HOST: string;
+                SMTP_PORT: string;
+                SMTP_USER: string;
+                SMTP_PASSWORD: string;
+                SECRET_HASH: string;
+                NODE_ENV: 'development' | 'production';
+                PORT?: string;
+                PWD: string;
+                PG_USER: string;
+                PG_PASSWORD: string;
+                PG_PORT: string;
+                PG_DATABASE:string;
+            }
+      }
+}
